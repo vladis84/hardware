f652bb0adb13633651620fd2106c8f13c65c7e43
Gena <Gena@gena-pc>
2017-08-15 11:27:49 +0600

    добавил bootstrap

--- web/index_dev.php
+++ web/index_dev.php
@@ -12,7 +12,12 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-require_once __DIR__.'/../vendor/autoload.php';
+// Сервер возвращает файлы напрямую.
+if (preg_match('/\.(png|jpg|jpeg|gif|css|js)$/', $_SERVER['REQUEST_URI'])) {
+    return false;    
+}
+
+require_once __DIR__ . '/../vendor/autoload.php';
 
 Debug::enable();
 
